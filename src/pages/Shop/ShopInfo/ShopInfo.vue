1d60e9022ceb541e7bdf797c2dd7cdfa75f3c3dd
next2014 <83329265@qq.com>
2020-03-12 21:51:05 +0800

    0312

--- src/pages/Shop/ShopInfo/ShopInfo.vue
+++ src/pages/Shop/ShopInfo/ShopInfo.vue
@@ -1,6 +1,68 @@
 <template>
-  <div>
-     ShopRatings
+  <div class="shop-info">
+    <div class="info-content">
+      <section class="section">
+        <h3 class="section-title">配送信息</h3>
+        <div class="delivery">
+          <div>
+            <span class="delivery-icon">硅谷专送</span>
+            <span>由商家配送提供配送，约 40 分钟送达，距离 100m</span>
+          </div>
+          <div class="delivery-money">配送费￥5</div>
+        </div>
+      </section>
+      <div class="split"></div>
+      <section class="section">
+        <h3 class="section-title">活动与服务</h3>
+        <div class="activity">
+          <div class="activity-item activity-green">
+            <span class="content-tag">
+              <span class="mini-tag">首单</span>
+            </span>
+            <span class="activity-content">新用户下单立减 17 元(不与其它活动同享)</span>
+          </div>
+          <div class="activity-item activity-red">
+            <span class="content-tag">
+              <span class="mini-tag">满减</span>
+            </span>
+            <span class="activity-content">满 35 减 19，满 65 减 35</span>
+          </div>
+          <div class="activity-item activity-orange">
+            <span class="content-tag">
+              <span class="mini-tag">特价</span>
+            </span>
+            <span class="activity-content">【立减 19.5 元】欢乐小食餐</span>
+          </div>
+        </div>
+      </section>
+      <div class="split"></div>
+      <section class="section">
+        <h3 class="section-title">商家实景</h3>
+        <div class="pic-wrapper">
+          <ul class="pic-list">
+            <li class="pic-item">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg" />
+            </li>
+            <li class="pic-item">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg" />
+            </li>
+            <li class="pic-item">
+              <img width="120" height="90" src="https://fuss10.elemecdn.com/f/7f/d1422ec824a0a9d1fb879c57ab533jpeg.jpeg" />
+            </li>
+          </ul>
+        </div>
+      </section>
+      <div class="split"></div>
+      <section class="section">
+        <h3 class="section-title">商家信息</h3>
+        <ul class="detail">
+          <li><span class="bold">品类</span> <span>包子粥店</span></li>
+          <li><span class="bold">商家电话</span> <span>13301083744</span></li>
+          <li><span class="bold">地址</span> <span>北京市丰台区</span></li>
+          <li><span class="bold">营业时间</span> <span>08:35-23:00</span></li>
+        </ul>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -9,5 +71,5 @@
 </script>
 
 <style lang="stylus" rel="styesheet/stylus">
-  
+
 </style>
