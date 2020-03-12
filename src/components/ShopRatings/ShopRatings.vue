103a38081f12ff8bb178c38f16ddaa20c7aa9b1f
next2014 <83329265@qq.com>
2020-03-12 10:47:41 +0800

    0312

---
+++ src/components/ShopRatings/ShopRatings.vue
@@ -0,0 +1,13 @@
+<template>
+  <dvi>
+    ShopRatings
+  </dvi>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  
+</style>
--- src/store/actions.js
+++ src/store/actions.js
@@ -6,14 +6,23 @@ import {
   RECEIVE_CATEGERYS,
   RECEIVE_SHOPS,
   RECEIVE_USER_INFO,
-  RESET_USER_INFO
+  RESET_USER_INFO,
+  RECEIVE_INFO,
+  RECEIVE_RATINGS,
+  RECEIVE_GOODS,
+  INCREMENT_FOOD_COUNT,
+  DECREMENT_FOOD_COUNT,
+  CLEAR_CART
 } from './mutation-types'
 import {
   reqAddress,
   reqFoodCategorys,
   reqShops,
   reqUserInfo,
-  reqLogout
+  reqLogout,
+  reqShopRatings,
+  reqShopGoods,
+  reqShopInfo
 } from '../api'
 
 export default {
@@ -66,13 +75,54 @@ export default {
     }
   },
 
-  // 异步登出
-  async logout ({commit}) {
-    const result = await reqLogout()
-    if(result.code===0) {
+   // 异步登出
+   async logout ({commit}) {
+     const result = await reqLogout()
+     if(result.code===0) {
        const userInfo = result.data
        commit(RESET_USER_INFO)
-    }
-  }
+     }
+   },
 
+   // 异步获取商家信息
+   async getShopInfo({commit}) {
+     const result = await reqShopInfo()
+     if(result.code===0) {
+       const info = result.data
+       commit(RECEIVE_INFO, {info})
+     }
+   },
+
+    // 异步获取商家评价列表
+    async getShopRatings({commit}) {      const result = await reqShopRatings()
+      if(result.code===0) {
+        const ratings = result.data
+        commit(RECEIVE_RATINGS, {ratings})
+      }
+    },
+
+    // 异步获取商家商品列表
+    async getShopGoods({commit}, callback) {
+       const result = await reqShopGoods()
+       if(result.code===0) {
+          const goods = result.data
+          commit(RECEIVE_GOODS, {goods})
+          // 数据更新了，通知一下组件
+          callback && callback ()
+        }
+    },
+
+    // 同步更新food中的count值
+    updateFoodCount ({commit}, {isAdd, food}) {
+      if(isAdd) {
+        commit(INCREMENT_FOOD_COUNT, {food})
+      } else {
+        commit(DECREMENT_FOOD_COUNT, {food})
+      }
+    },
+
+    // 同步清空购物车
+    clearCart ({commit}) {
+       commit(CLEAR_CART)
+    }
 }
